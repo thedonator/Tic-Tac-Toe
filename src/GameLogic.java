de0f180a58ebdc75387503f057bb7f0f80460ede
thedonator <thedonator@users.noreply.github.com>
2021-03-24 20:29:56 +0100

    Add documentation for GameLogic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -2,16 +2,35 @@ import java.util.Scanner;
 
 public class GameLogic {
 
+
+  /**
+   * 
+   * @param in is the input stream
+   * @return returns true if the move in input is valid
+   */
   public static boolean isValidXMove(Scanner in)
   {
     return Board.xMove >= 0 && Board.xMove < 4;
   }
 
+
+  /**
+   * 
+   * @param in is the input stream
+   * @return returns true if the move in input is valid
+   */
   public static boolean isValidYMove(Scanner in)
   {
     return Board.yMove >= 0 && Board.yMove < 4;
   }
 
+  /**
+   * The function checks if the current move goes in a cell that is 
+   * already full
+   * 
+   * @param in is the input stream
+   * @return returns true if the move in input is valid
+   */
   public static boolean hasTheRightsToBeMoved(Scanner in)
   {
     return Board.isEmpty[Board.yMove][Board.xMove];
